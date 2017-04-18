501f04012b6259b6deebc795a88cef26f8f7bb30
Tom Patzig <tom.patzig@sap.com>
2017-04-18 11:12:04 +0000

    use admin net as default gateway

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -1061,6 +1061,7 @@ EOF
         -e "s/ 300/ $vlan_public/g" \
         -e "s/ 500/ $vlan_fixed/g" \
         -e "s/ [47]00/ $vlan_sdn/g" \
+        -e "s/\"router_pref\": 5/\"router_pref\": 15/g" \
         $netfile
 
     # set requested networking mode for Crowbar
